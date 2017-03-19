26baf8d4ddef24e2ecade49eda26a69ac0ac0533
Raymond Kim <raymond.sj.kim@gmail.com>
2017-03-18 23:48:12 -0700

    Create length_function.cpp

---
+++ public/ch_02/length_function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+	string name = "Raymond";
+	cout << name.length() << endl
+		<< name.substr(0,5) << endl;
+
+
+	system("pause");
+	return 0;
+}
