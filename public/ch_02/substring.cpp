7e5fa897ab3ed6879d04597cb11d1384fda89280
Raymond Kim <raymond.sj.kim@gmail.com>
2017-03-18 23:51:56 -0700

    Create substring.cpp

---
+++ public/ch_02/substring.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter your first name: ";
+	string first;
+	cin >> first;
+
+	cout << "Enter your significant other's first name: ";
+	string second;
+	cin >> second;
+
+	string initials = first.substr(0,1) + "&" + second.substr(0,1);
+
+	cout << initials << endl;
+
+	system("pause");
+	return 0;
+}
