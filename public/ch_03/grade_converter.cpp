f8f23ca15556646fd4947e480c6630e25c3a3873
Raymond Kim <raymond.sj.kim@gmail.com>
2017-03-28 08:38:17 -0700

    Update grade_converter.cpp

--- public/ch_03/grade_converter.cpp
+++ public/ch_03/grade_converter.cpp
@@ -8,82 +8,82 @@ int main()
 	double num_grade = 0;
 	string letter_grade;
 
-	cout << "Enter a valid letter grade (Example: A+) : ";
+	cout << "Enter a valid letter grade (Example: A+ or a+) : ";
 	cin >> letter_grade;
 
-	if (letter_grade == "A+")
+	if (letter_grade == "A+" || letter_grade == "a+")
 	{
 		num_grade = 4.0;
 	}
 	else
 	{
-		if (letter_grade == "A")
+		if (letter_grade == "A" || letter_grade == "a")
 		{
-			num_grade = 4.0 - 0.15;
+			num_grade = 4.0;
 		}
 		else
 		{
-			if (letter_grade == "A-")
+			if (letter_grade == "A-" || letter_grade == "a-")
 			{
 				num_grade = 4.0 - 0.3;
 			}
 			else
 			{
-				if (letter_grade == "B+")
+				if (letter_grade == "B+" || letter_grade == "b+")
 				{
-					num_grade = 3.0;
+					num_grade = 3.0 + 0.3;
 				}
 				else
 				{
-					if (letter_grade == "B")
+					if (letter_grade == "B" || letter_grade == "b")
 					{
-						num_grade = 3.0 - 0.15;
+						num_grade = 3.0;
 					}
 					else
 					{
-						if (letter_grade == "B-")
+						if (letter_grade == "B-" || letter_grade == "b-")
 						{
 							num_grade = 3.0 - 0.3;
 						}
 						else
 						{
-							if (letter_grade == "C+")
+							if (letter_grade == "C+" || letter_grade == "c+")
 							{
-								num_grade = 2.0;
+								num_grade = 2.0 + 0.3;
 							}
 							else
 							{
-								if (letter_grade == "C")
+								if (letter_grade == "C" || letter_grade == "c")
 								{
-									num_grade = 3.0 - 0.15;
+									num_grade = 3.0;
 								}
 								else
 								{
-									if (letter_grade == "C-")
+									if (letter_grade == "C-" || letter_grade == "c-")
 									{
 										num_grade = 2.0 - 0.3;
 									}
 									else
 									{
-										if (letter_grade == "D+")
+										if (letter_grade == "D+" || letter_grade == "d+")
 										{
-											num_grade = 1.0;
+											num_grade = 1.0 + 0.3;
 										}
 										else
 										{
-											if (letter_grade == "D")
+											if (letter_grade == "D" || letter_grade == "d")
 											{
-												num_grade = 1.0 - 0.15;
+												num_grade = 1.0;
 											}
 											else
 											{
-												if (letter_grade == "D-")
+												if (letter_grade == "D-" || letter_grade == "d-")
 												{
 													num_grade = 1.0 - 0.3;
 												}
 												else
 												{
-													if (letter_grade == "F")
+													if (letter_grade == "F" || letter_grade == "f")
 													{
 														num_grade = 0;
 													}
