6b5c287af9c6f4619d4e9d8521beb23e6b9da0b8
Raymond Kim <raymond.sj.kim@gmail.com>
2017-03-30 08:52:06 -0700

    Update if_statement.cpp

--- public/ch_03/if_statement.cpp
+++ public/ch_03/if_statement.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
+	
 	int floor;
-	cout << "Enter a floor: ";
+	cout << "Enter a floor #: ";
 	cin >> floor;
 
-	int actual_floor;
-
-	if(floor > 13) {
-		actual_floor = floor -1;
-	} else {
-		actual_floor = floor;
+	// check for input errors
+	if(cin.fail())
+	{
+		cout << "Error: Input was not an integer." << endl;
+		system("pause");
+		return 1;
+	}
+	if(floor == 13)
+	{
+		cout <<"Error: There is no 13 floor" << endl;
+		system("pause");
+		return 1;
+	}
+	if(floor <= 0 || floor > 20)
+	{
+		cout << "Error: The floor must be between 1-20." << endl;
+		system("pause");
+		return 1;
 	}
 
-	cout << "The elevator will travel to the actual floor: " << actual_floor << endl;
+	// Actual floor
+	int actual_floor;
+
+	if(floor > 13)
+	{
+		actual_floor = floor - 1;
+	}
+	else
+	{
+		actual_floor = floor;
+	}
+	cout << "Elevator will travel to the actual floor: " << actual_floor << endl;
+
 
 	system("pause");
 	return 0;
