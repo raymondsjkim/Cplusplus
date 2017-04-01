9aefbf08ff2f45d4402e248de037e559664697f1
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-01 00:59:22 -0700

    Create simple_while_loop.cpp

---
+++ public/ch_04/simple_while_loop.cpp
@@ -0,0 +1,21 @@
+/*
+	A very simple while loop.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int start = 0;
+	int end = 10;
+
+	while (start < end) {
+		start++;
+		cout << start << endl;
+	}
+
+	system("pause");
+    return 0;
+}
