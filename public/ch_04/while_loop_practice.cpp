07b2bad9b005a88cf3f580a38e304c5ed7b522be
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-06 08:51:29 -0700

    Create while_loop_practice.cpp

---
+++ public/ch_04/while_loop_practice.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	double sum = 0;
+	int count = 0;
+	double salary = 0;
+
+	cout << "Enter salaries, -1 to finish: ";
+	while(salary != -1)
+	{
+		cin >> salary;
+		if(salary != -1)
+		{
+			sum = sum + salary;
+			count++;
+		}
+	}
+	if(count > 0)
+	{
+		double average = sum / count;
+		cout << "Average salary: " << average << endl;
+	}
+	else
+	{
+		cout << "No data" << endl;
+	}
+	
+
+	system("pause");
+	return 0;
+}
