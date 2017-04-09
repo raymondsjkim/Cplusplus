57849e9b73c51e91c6bd177138d8120b60833ad3
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-09 16:57:07 -0700

    Update multiplication_table.cpp

--- public/lab/multiplication_table.cpp
+++ public/lab/multiplication_table.cpp
@@ -7,17 +7,18 @@ using namespace std;
 int main()
 {
 	int head_max = 10;
-	int row_max = 10;
+	int	row_max = 10;
 	int i;
 
 	for (i = 1; i <= head_max; i++) {
-		cout << i << setw(10) << " ";
+		cout << setw(4) << i << setw(4);
 	}
 	cout << endl;
 
 	for (int r = 2; r <= row_max; r++) {
+		cout << setw(4);
 		for (int mult = 1; mult <= row_max; mult++) {
-			cout << mult * r << setw(10) << " ";
+			cout << mult * r << setw(4);
 		}
 		cout << endl;
 	}
