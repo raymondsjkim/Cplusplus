78308894c8b2806facf86f2a8696ac0336145620
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-09 18:34:42 -0700

    Update multiplication_table.cpp

--- public/lab/multiplication_table.cpp
+++ public/lab/multiplication_table.cpp
@@ -22,17 +22,17 @@ using namespace std;
 int main()
 {
 	int head_max = 10;
-	int	row_max = 10;
+	int row_max = 10;
 	int i;
 	// Print header row.
 	for (i = 1; i <= head_max; i++) {
 		cout << setw(4) << i << setw(4);
 	}
 	cout << endl;
-	// Print body column
+	// Print table body
 	for (int r = 2; r <= row_max; r++) {
 		cout << setw(4);
-		// Print multiplication table
+		// Print multiplication table rows
 		for (int mult = 1; mult <= row_max; mult++) {
 			cout << mult * r << setw(4);
 		}
