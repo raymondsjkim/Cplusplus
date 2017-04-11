65ca7bce260a6ddfa9b50335fb1a349dbf9661b1
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-11 08:49:56 -0700

    Update rand_function.cpp

--- public/ch_04/rand_function.cpp
+++ public/ch_04/rand_function.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	// Print random number between 10 and 100.
-	srand(time(0));
+	srand(time(0)); // seed rand() to produce different results.
 	int i = rand() % 90 + 10;
 	cout << i << endl;
 
