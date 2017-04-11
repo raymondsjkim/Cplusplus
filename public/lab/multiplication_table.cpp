2ce5659a8c8b063d469ece5e180d52791c555892
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-11 08:38:15 -0700

    Update multiplication_table.cpp

--- public/lab/multiplication_table.cpp
+++ public/lab/multiplication_table.cpp
@@ -1,15 +1,11 @@
 /*
 Description: This program prints a multiplication table.
-
 Author: Raymond Kim
-
 Input Variables: a) head_max b) row_max c) i d) r
-
 Process Flow: For i is 1 and is less than or equal to head_max, increment by one (prints header row).
 For r is 2 and is less than or equal to row_max, increment by 1 (prints first column) 
 but (note: nested for loop) For mult is 1 and is less than or equal to row_max, increment by 1 
 while multiplying mult * r (calculate and print the multiplaction table).
-
 Output Variable: Multiplication table
 */
 #include "stdafx.h"
@@ -21,19 +17,17 @@ Output Variable: Multiplication table
 using namespace std;
 int main()
 {
-	int head_max = 10;
-	int row_max = 10;
-	int i;
+	const int MAX = 10;
+
 	// Print header row.
-	for (i = 1; i <= head_max; i++) {
+	for (int i = 1; i <= MAX; i++) {
 		cout << setw(4) << i << setw(4);
 	}
 	cout << endl;
 	// Print table body
-	for (int r = 2; r <= row_max; r++) {
-		cout << setw(4);
+	for (int r = 2; r <= MAX; r++) {
 		// Print multiplication table rows
-		for (int mult = 1; mult <= row_max; mult++) {
+		for (int mult = 1; mult <= MAX; mult++) {
 			cout << mult * r << setw(4);
 		}
 		cout << endl;
