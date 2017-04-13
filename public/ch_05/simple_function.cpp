585a9203550b62624c476b96c5aadbe3b33af26c
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-13 08:58:44 -0700

    Update simple_function.cpp

--- public/ch_05/simple_function.cpp
+++ public/ch_05/simple_function.cpp
@@ -8,8 +8,12 @@ double cube_volume(double side_length) {
 
 int main()
 {
-	double z = cube_volume(2);
-	cout << z << endl;
+	double result1 = cube_volume(2);
+	double result2 = cube_volume(10);
+	cout << "A cube with side length 2 has a volume " 
+		<< result1 << endl;
+	cout << "A cube with side length 10 has a volume " 
+		<< result2 << endl;
 
 	system("pause");
 	return 0;
