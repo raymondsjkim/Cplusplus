52620a3da6d3d94babef5bd03e62367280659f08
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-18 09:36:44 -0700

    Update correct_exam3.cpp

--- public/ch_04/correct_exam3.cpp
+++ public/ch_04/correct_exam3.cpp
@@ -3,28 +3,32 @@ using namespace std;
 
 int main()
 {
-	double min = 0;
+	double min = 9999;
 	double max = 0;
+	double avg = 0;
 	double sum = 0;
 	double count = 0;
-	double avg = 0;
+	double variable = 0;
 
-	cout << "Enter values: ";
-	double values;
-	
-	// http://stackoverflow.com/questions/7425318/multiple-inputs-on-one-line
-	
-	while(cin >> values) 
-	{
+	cout << "Enter some values: ";
+
+	while(cin >> variable) {
 		if(cin.fail()) {
-			if(max > values) {
-				max = values;
-			}
+			break;
 		}
+		if(variable > max) {
+			max = variable;
+		}
+		if(variable < min) {
+			min = variable;
+		}
+		++count;;
+		sum = sum + variable;
 	}
-	cout << max << endl;
+	
+	avg = max / min;
 
-
-	system("pause");
-	return 0;
-}
+	cout << "Max: " << max << endl;
+	cout << "Min: " << min << endl;
+	cout << "Average: " << avg << endl;
+	cout << "Range: " << count << endl;
