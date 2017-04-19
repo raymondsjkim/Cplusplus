f69417f483bfe3d4a5bf98ce55ef46609e9bc2a7
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-19 13:05:13 -0700

    Update write_functions.cpp

--- public/lab/write_functions.cpp
+++ public/lab/write_functions.cpp
@@ -8,12 +8,12 @@ using namespace std;
 /**
 Computes the volume of a sphere.
 @param r is the radius of the sphere.
-@return the volume
+@return the volume_sphere
 */
 double sphere_volume(double r) {
 	const double PI = M_PI;
-	double sphere_vol = 4.0 / 3 * PI * pow(r, 3);
-	return sphere_vol;
+	double volume_sphere = 4.0 / 3 * PI * pow(r, 3);
+	return volume_sphere;
 }
 /**
 Computes the surface area of a sphere.
@@ -26,6 +26,17 @@ double sphere_surface(double r) {
 	return sphere_area;
 }
 
+/**
+Computes the volume of a cylinder.
+@param r and h. r is radius h is height.
+@return cyl_vol
+*/
+double cylinder_volume(double r, double h) {
+	const double PI = M_PI;
+	double cyl_vol = PI * pow(r, 2) * h;
+	return cyl_vol;
+}
+
 int main()
 {
 	cout << "Enter values for radius and height and hit enter \nkey to execute the program (example: 2 4; r=2 & h=4): "
@@ -34,9 +45,9 @@ int main()
 	double height;
 	cin >> radius;
 	cin >> height;
-
 	cout << "Volume of the sphere is: " << sphere_volume(radius) << endl;
 	cout << "Surface area of the sphere is: " << sphere_surface(radius) << endl;
+	cout << "Volume of the cylinder is: " << cylinder_volume(radius, height) << endl;
 
 	system("pause");
 	return 0;
