8918b4b39bcea2a2689ef1344b5162cc38fcb2b9
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-20 09:26:36 -0700

    Create reference_parameters.cpp

---
+++ public/ch_05/reference_parameters.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void withdraw(double& balance, double amount) { // the & triggers to change in the account
+	const double PENALTY = 10;
+	if(balance >= amount) {
+		balance = balance - amount;
+	} else {
+		balance = balance - PENALTY;
+	}
+}
+
+int main()
+{
+	double harrys_account = 1000;
+	double sallys_account = 500;
+	
+	withdraw(harrys_account, 100);
+		// Now harrys_account is 900
+	withdraw(harrys_account, 1000);
+		// Now harrys_account is 890
+	withdraw(sallys_account, 150);
+	
+	cout << "Harry's account: " << harrys_account << endl;
+	cout << "Sally's account: " << sallys_account << endl;
+
+	system("pause");
+	return 0;
+}
