374655126dc24c220fc676c019c2dc4366c50f55
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-20 09:47:15 -0700

    Update write_functions.cpp

--- public/lab/write_functions.cpp
+++ public/lab/write_functions.cpp
@@ -2,6 +2,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -55,19 +56,33 @@ double cone_volume(double r, double h) {
 	double cone_vol = PI * pow(r,2) * (h / 3);
 	return cone_vol;
 }
+
+/**
+Computes surface area of a cone.
+@param r and h. r is radius h is height.
+@return cone_vol
+*/
+double cone_surface(double r, double h) {
+	const double PI = M_PI;
+	double cone_vol = PI * r * (r + sqrt(h * h + r * r));
+	return cone_vol;
+}
 int main()
 {
-	cout << "Enter values for radius and height and hit enter \nkey to execute the program (example: 2 4; r=2 & h=4): "
-		<< " ";
+	cout << "Enter values for radius and height and hit enter \nkey to execute the program (example: 2 4; r=2 & h=4): ";
 	double radius;
 	double height;
 	cin >> radius >> height;
 	
+	cout << endl;
 	cout << "Volume of the sphere is: " << sphere_volume(radius) << endl;
 	cout << "Surface area of the sphere is: " << sphere_surface(radius) << endl;
+	cout << endl;
 	cout << "Volume of the cylinder is: " << cylinder_volume(radius, height) << endl;
 	cout << "Surface area of the cylinder is: " << cylinder_surface(radius, height) << endl;
+	cout << endl;
 	cout << "Volume of the cone is: " << cone_volume(radius, height) << endl;
+	cout << "Surface area of the cone is: " << cone_volume(radius, height) << endl;
 
 	system("pause");
 	return 0;
