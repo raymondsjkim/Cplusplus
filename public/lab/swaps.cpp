95990e826809f56c04e7e50d5f553ede020fed5c
Raymond Kim <raymond.sj.kim@gmail.com>
2017-04-23 19:44:56 -0700

    Update swaps.cpp

--- public/lab/swaps.cpp
+++ public/lab/swaps.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 void sort2(int& a, int b)
 {
-	if (a > b) {
-		b = a;
-	}
+	cout << "a is: " << a << ", "<< "b is: " << b << endl;
 }
 int main()
 {
@@ -13,13 +11,13 @@ int main()
 	int v = 3;
 	int w = 4;
 	int x = 1;
-
 	//   (a,b)
 	//   (4,1)
 	sort2(w, x); // swap to (1,4)
 	cout << "w is now " << w << ", " << "x is now " << x << endl; 
-  
+
 	system("pause");
     return 0;
 }
+
 
