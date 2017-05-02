6adf3278950d5ff707601184fe56937aee83e69b
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-02 09:21:44 -0700

    Create sorting.cpp

---
+++ public/ch_06/sorting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm> // include this to sort
+using namespace std;
+
+int main()
+{
+
+	double values[] = {32, 54, 67.5, 29, 35, 80, 115, 44.5, 100, 65};
+	int current_size = 10;
+	
+	sort(values, values + current_size);
+
+	for(int i = 1;i < current_size; i++)
+	{
+		cout << values[i];
+		cout << endl;
+	}
+
+	system("pause");
+    return 0;
+}
