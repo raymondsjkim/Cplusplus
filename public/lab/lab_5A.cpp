debe698c928d1ac6eaf84554f7cf8b118baa2902
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-08 18:57:38 -0700

    Update lab_5A.cpp

--- public/lab/lab_5A.cpp
+++ public/lab/lab_5A.cpp
@@ -13,6 +13,13 @@ Output Variable: Same or not the same arrays.
 #include <string>
 using namespace std;
 
+
+/**
+Check to see if the values in the array are the same.
+@param a and b arrays containing numbers
+@param a_size and b_size the capacity of the arrays
+return true if the array is the same else return false.
+*/
 bool equals(int a[], int a_size, int b[], int b_size)
 {
 	if (a_size == b_size) // continue if the array size is the same else return false
@@ -38,11 +45,11 @@ int main()
 	int b[MAX] = { 1,2,3,4,5,6,7,8,9,10 };
 	bool equal = equals(a, MAX, b, MAX);
 	if (equal) {
-		cout << "Both arrays are the same" << endl;
+		cout << "Both arrays are the same" << endl; // if returns true
 	}
 	else
 	{
-		cout << "Two arrays are different" << endl;
+		cout << "Two arrays are different" << endl; // if returns false
 	}
 	system("pause");
 	return 0;
