b3260dbcf5ccfa76084556e90ca3a1bf910246c7
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-08 18:47:03 -0700

    Create lab_5A.cpp

---
+++ public/lab/lab_5A.cpp
@@ -0,0 +1,49 @@
+/*
+Description: This program checks whether two arrays have the same elements in order.
+Author: Raymond Kim
+Input Variables: MAX, a[MAX], b[MAX], equal
+Process Flow: Create two arrays with a size of 10.
+			  Create a funtion equals() with 4 parameters for 2 arrays and 2 sizes.
+			  If the size of the array are the same check if the values in the array are not the same.
+			  If they are not the same return false, else return true.
+Output Variable: Same or not the same arrays.
+*/
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool equals(int a[], int a_size, int b[], int b_size)
+{
+	if (a_size == b_size) // continue if the array size is the same else return false
+	{
+		for (int i = 0; i < a_size && b_size; i++)
+		{
+			if (a[i] != b[i]) // if a value does not match return false
+			{
+				return false;
+			}
+		}
+		return true; 
+	}
+	else {
+		return false;
+	}
+}
+
+int main()
+{
+	const int MAX = 10;
+	int a[MAX] = { 1,2,3,4,5,6,7,8,9,10 };
+	int b[MAX] = { 1,2,3,4,5,6,7,8,9,10 };
+	bool equal = equals(a, MAX, b, MAX);
+	if (equal) {
+		cout << "Both arrays are the same" << endl;
+	}
+	else
+	{
+		cout << "Two arrays are different" << endl;
+	}
+	system("pause");
+	return 0;
+}
