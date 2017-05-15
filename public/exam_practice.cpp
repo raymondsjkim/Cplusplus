2ec771d54cb5f296e1e6fede0fe7aac97299320b
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-15 12:50:52 -0700

    Update exam_practice.cpp

--- public/exam_practice.cpp
+++ public/exam_practice.cpp
@@ -1,20 +1,18 @@
+#include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-/**
-Check to see if the values in the array are the same.
-@param a and b arrays containing numbers
-@param a_size and b_size the capacity of the arrays
-return true if the array is the same else return false.
-*/
 bool same_set(int a[], int a_size, int b[], int b_size)
 {
-	if (a_size == b_size) // continue if the array size is the same else return false
+	sort(a, a + a_size);
+	sort(b, b + b_size);
+	if (a_size == b_size)
 	{
 		for (int i = 0; i < a_size && b_size; i++)
 		{
-			if (a[i] != b[i]) // if a value does not match return false
+			if (a[i] != b[i]) 
 			{
 				return false;
 			}
@@ -28,13 +26,13 @@ bool same_set(int a[], int a_size, int b[], int b_size)
 }
 int main()
 {
-	const int MAX = 5;
+	const int MAX = 9;
 	int a[MAX];
 	int b[MAX];
 	int input_a;
 	int input_b;
 	cout << endl;
-	cout << "Enter 5 numbers for array a: ";
+	cout << "Enter 9numbers for array a: ";
 	for (int i = 0; i < MAX; i++)
 	{
 		if (cin >> input_a)
@@ -42,7 +40,7 @@ int main()
 			a[i] = input_a;
 		}
 	}
-	cout << "Enter 5 numbers for array b: ";
+	cout << "Enter 9umbers for array b: ";
 	for (int j = 0; j < MAX; j++)
 	{
 		if (cin >> input_b)
@@ -51,6 +49,7 @@ int main()
 		}
 	}
 	cout << endl;
+
 	bool same = same_set(a, MAX, b, MAX);
 	if (same) {
 		cout << "TRUE: Both unsorted arrays have the same muliplicities" << endl; // if returns true
