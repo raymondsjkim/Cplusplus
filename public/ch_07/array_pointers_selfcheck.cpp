9a634d26180491ca283f6efeb9e5ba9309605e31
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-23 10:25:07 -0700

    Update array_pointers_selfcheck.cpp

--- public/ch_07/array_pointers_selfcheck.cpp
+++ public/ch_07/array_pointers_selfcheck.cpp
@@ -20,3 +20,21 @@ int main()
 	system("pause");
 	return 0;
 }
+
+/*
+	int a[] = {2,3,5};
+	
+	int* p = a;
+	cout << *p << " ";
+	
+	int* q = a + 2;
+	p++;
+	cout << *p << " ";
+	
+	q--;
+	*p = *q;
+	p++;
+	cout << *p << endl;
+	// output: 2 3 5
+
+*/
