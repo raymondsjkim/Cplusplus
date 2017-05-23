34ce0ab0f4e2c3ed5c1834f6e73e97ce705b02a4
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-23 08:27:50 -0700

    Update pointer_array.cpp

--- public/ch_07/pointer_array.cpp
+++ public/ch_07/pointer_array.cpp
@@ -29,6 +29,25 @@ int main()
 	int a[10] = { 0,1,4,9,16,25,36,49,64,81 };
 	int* p = a;
 	cout << *(p + 8) << endl; // output: 64
+*/
 
 
+/*
+void sum(int* a, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(a + i) << endl;
+	}
+}
+int main()
+{
+	const int MAX = 10;
+	int data[MAX] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	sum(data, MAX);
+
+	system("pause");
+	return 0;
+}
 */
