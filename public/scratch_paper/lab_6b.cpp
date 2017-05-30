6e033c9b8075bd7000fd141bfe8ad33b627a6a55
Raymond Kim <raymond.sj.kim@gmail.com>
2017-05-29 18:24:48 -0700

    Update lab_6b.cpp

--- public/scratch_paper/lab_6b.cpp
+++ public/scratch_paper/lab_6b.cpp
@@ -4,30 +4,41 @@ using namespace std;
 
 int* read_data(int& size)
 {
-	// two arrays will be in here
-
-
-	// read input
-	// fill up array_ptr
-	// find out the size array_ptr[i]
-
-	// if size > MAX allocate a new array
-	// int* bigger_array = new int[2 * size] p.326
-	// copy first_array to bigger_array p.326
-	// continue to read & fill the bigger_array p.326
-	// end if 
-		// delete[] first_array
-	// print bigger_array
+	int temp_size = 10;
+	int* temp = new int[temp_size];
+	size = 0;
+	int input = 0;
+	while (cin >> input)
+	{
+		temp[size] = input;
+		size++;
+		if (size == temp_size)
+		{
+			int* new_temp = new int[temp_size * 2];
+			for (int i = 0; i < temp_size; i++)
+			{
+				new_temp[i] = temp[i];
+			}
+			temp_size = temp_size * 2;
+			delete[] temp;
+		}
+	}
+	return temp;
 }
-
 
 int main()
 {
-	const int MAX = 10;
-	double first_array[MAX];
-
-	int* array_ptr = new int[MAX];
+	int size;
+	int* values;
+	cout << "Enter: ";
+	values = read_data(size);
+	cout << "The inputs are: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << values[i] << " ";
+	}
+	cout << endl;
 
 	system("pause");
-	return 0;
+    return 0;
 }
