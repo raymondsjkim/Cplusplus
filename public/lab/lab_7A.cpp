9a9cb728d4694f29fa44ea5d21f9faa73392aba6
Raymond Kim <raymond.sj.kim@gmail.com>
2017-06-01 10:15:00 -0700

    Update lab_7A.cpp

--- public/lab/lab_7A.cpp
+++ public/lab/lab_7A.cpp
@@ -5,34 +5,32 @@
 
 using namespace std;
 
-void process_name(ifstream& in_file, double& total)
+void process_name(ifstream& in_file, ofstream& out_file, double& total)
 {
 	string name;
 	int count;
 	double percent;
 	in_file >> name >> count >> percent;
 
-	if (in_file.fail())
-	{
-		return;
-	}
 	if (total > 0)
 	{
 		cout << name << " ";
+		out_file << name << " " << count << endl;
 	}
 	total = total - percent;
 }
 
 int main()
 {
-
 	ifstream in_file;
-	in_file.open("C:\\Users\\raymo\\OneDrive\\Documents\\babynames.txt");
-
+	ofstream out_file;
+	in_file.open("s:\\babynames.txt");
+	
 	if (in_file.fail())
 	{
 		return 0;
 	}
+	out_file.open("s:\\output.txt");
 	double boy_total = 50;
 	double girl_total = 50;
 
@@ -45,8 +43,8 @@ int main()
 			return 0;
 		}
 		cout << rank << " ";
-		process_name(in_file, boy_total);
-		process_name(in_file, girl_total);
+		process_name(in_file, out_file, boy_total);
+		process_name(in_file, out_file, girl_total);
 		cout << endl;
 	}
 	system("pause");
