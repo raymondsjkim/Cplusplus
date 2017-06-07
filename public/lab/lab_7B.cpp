4e6f83eacb423f906113662cda845e9814e5869a
Raymond Kim <raymond.sj.kim@gmail.com>
2017-06-06 22:23:36 -0700

    Update lab_7B.cpp

--- public/lab/lab_7B.cpp
+++ public/lab/lab_7B.cpp
@@ -4,34 +4,38 @@
 #include <string>
 using namespace std;
 
+void get_avg(ifstream& in_file, ofstream& out_file, int& count)
+{
+	double col1, col2;
+	double sum = 0;
+
+	while (in_file >> col1 >> col2)
+	{
+		out_file << col1 << " " << col2 << endl;
+		count++;
+
+		sum = count + col1;
+	}
+	cout << sum << endl;
+}
+
 int main()
 {
-
-	cout << "Please enter a file name: ";
-	string filename;
-	cin >> filename;
-	ifstream in_file;
-	in_file.open(filename.c_str());
-	ofstream out_file;
-
-	double column1;
-	double column2;
 	int count = 0;
-
-
-	in_file.open("s:\\input.txt");
-	if(in_file.fail())
+	
+	ifstream in_file;
+	ofstream out_file;
+	
+	in_file.open("f:\\data.txt");
+	if (in_file.fail())
 	{
 		cout << "In_file has failed" << endl;
+		system("pause");
 		return 0;
 	}
+	out_file.open("f:\\data_out.txt");
 	
-	while(in_file >> column1 >> column2)
-	{
-		out_file << column1 << column2;
-		count++;
-	}
-
+	get_avg(in_file, out_file, count);
 
 
 	system("pause");
